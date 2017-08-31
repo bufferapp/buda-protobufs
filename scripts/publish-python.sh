d79479aaae79eaf9a84dd73b14989038fb4b067f
David Gasquez <davidgasquez@gmail.com>
2017-08-31 16:14:43 +0200

    Trigger build

--- scripts/publish-python.sh
+++ scripts/publish-python.sh
@@ -5,8 +5,7 @@ set -eo pipefail
 # Grab the version number
 VERSION=$(cat VERSION)
 
+# Upload package to Pypi
 cd packages/python
-
 python setup.py sdist
-
 twine upload dist//buda-$VERSION.tar.gz

642965c9d1588265e7c8e349fdcfbb59568b3c00
Dan Farrelly <daniel.j.farrelly@gmail.com>
2018-01-11 18:26:40 -0500

    Fix for automation

--- scripts/package-node.sh
+++ scripts/package-node.sh
@@ -4,4 +4,4 @@ budapest/run -c concat -i $PWD/buda -o $PWD/packages/node/
 # Update the version in the package.json
 VERSION=$(<VERSION)
 cd $PWD/packages/node/
-npm version $VERSION
+npm version $VERSION --allow-same-version

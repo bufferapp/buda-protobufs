7d650273c64efbbb933cee7a6b00a2727fb64947
Harrison Harnisch <hharnisc@gmail.com>
2018-11-21 09:35:08 -0600

    add back exporting messages for type verification

--- packages/node/index.js
+++ packages/node/index.js
@@ -1,11 +1,17 @@
 const path = require('path')
 const protoLoader = require('@grpc/proto-loader')
 const grpc = require('@grpc/grpc-js')
+const protobuf = require('protobufjs')
 
-const packageDefinition = protoLoader.loadSync(path.join(__dirname, 'buda.proto'))
-const packageObject = grpc.loadPackageDefinition(packageDefinition);
+const protoPath = path.join(__dirname, 'buda.proto')
+const packageDefinition = protoLoader.loadSync(protoPath)
+// loads all of the message types for verification
+const protobufLoader = protobuf.loadSync(protoPath)
+const packageObject = grpc.loadPackageDefinition(packageDefinition)
 
 module.exports = {
+  ...protobufLoader.nested.buda.nested,
   EventsCollector: packageObject.buda.EventsCollector,
   credentials: grpc.credentials.createInsecure(),
+  grpc,
 }
--- packages/node/index.test.js
+++ packages/node/index.test.js
@@ -1,9 +1,10 @@
 const assert = require('assert')
-const { EventsCollector, credentials } = require('./index')
+const { EventsCollector, Visit, credentials } = require('./index')
 
 // Very basic test to check that messages and services are loaded correctly
 assert.ok(EventsCollector)
 assert.ok(credentials)
+assert.ok(Visit)
 
 const client = new EventsCollector('localhost:50051', credentials)
 assert.ok(client.CollectVisit)
